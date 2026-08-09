67391988d47abc7caab16cfa647df3c88de56939
qiao-666-Eng <1396006442@qq.com>
2026-08-09 17:59:33 +0800

    项目初始化

---
+++ backend/src/main/java/com/enterprise_ai/EnterpriseAiApplication.java
@@ -0,0 +1,13 @@
+package com.enterprise_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnterpriseAiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/enterprise_ai/EnterpriseAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enterprise_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterpriseAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
